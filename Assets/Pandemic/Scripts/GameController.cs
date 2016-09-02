2c4ecc4353f2436ba7a4c03468ee305923c0844a
jiuya <tatsuya.yamamoto.kbhit@gmail.com>
2016-09-02 14:01:42 +0900

    init commit

---
+++ Assets/Pandemic/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+
+    private PhotonView photonView;
+
+    void Start()
+    {
+        photonView = PhotonView.Get(this);
+    }
+
+    void Update()
+    {
+        if (photonView.isMine)
+        {
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+            transform.Translate(x * 0.2f, 0, z * 0.2f);
+        }
+    }
+}
---
+++ Assets/Pandemic/Scripts/PhotonManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhotonManager : Photon.MonoBehaviour
+{
+    public string objectName;
+
+    void Start()
+    {
+        // Photonへの接続を行う
+        PhotonNetwork.ConnectUsingSettings("0.1");
+
+        // PhotonNetworkの更新回数のセット
+        PhotonNetwork.sendRate = 30;
+    }
+
+    /// <summary>
+    /// ロビーに接続すると呼ばれるメソッド
+    /// </summary>
+    void OnJoinedLobby()
+    {
+        // ランダムでルームに入室する
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    /// <summary>
+    /// ランダムで部屋に入室できなかった場合呼ばれるメソッド
+    /// </summary>
+    void OnPhotonRandomJoinFailed()
+    {
+        // ルームを作成、部屋名は今回はnullに設定
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    /// <summary>
+    /// ルームに入室成功した場合呼ばれるメソッド
+    /// </summary>
+    void OnJoinedRoom()
+    {
+        GameObject cube = PhotonNetwork.Instantiate(objectName, Vector3.zero, Quaternion.identity, 0);
+    }
+
+    /// <summary>
+    /// UnityのGameウィンドウに表示させる
+    /// </summary>
+    void OnGUI()
+    {
+        // Photonのステータスをラベルで表示させています
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+}
